b675bca349fd109c0013cc7ccc2e96aa5951daf6
Cleonildo Junior <cleu.junior@gmail.com>
2022-01-18 10:09:43 -0300

    Teste com a Struct Item

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,6 +2,8 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
+  alias Exlivery.Orders.Item
+
 
   def user_factory do
     %User{
@@ -11,6 +13,17 @@ defmodule Exlivery.Factory do
       age: 31,
       address: "Rua dos Pardais, 345"
     }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza de banana",
+      category: :pizza,
+      quantity: 1,
+      unity_price: Decimal.new("35.5")
+
+    }
+
   end
 
 end
